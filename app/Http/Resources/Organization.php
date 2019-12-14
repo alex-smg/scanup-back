fff6faa56d5f44316f575337ca2ba02bcb570269
j.bourel.ext_azu <calaruni@hotmail.com>
2019-12-14 17:35:43 +0100

    feat(organizations): Update organization resource for get parent organization

--- app/Http/Resources/Organization.php
+++ app/Http/Resources/Organization.php
@@ -4,6 +4,8 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
+use App\Organization as OrganizationEntity;
 
 class Organization extends JsonResource
 {
@@ -21,6 +23,8 @@ class Organization extends JsonResource
             'description' => $this->description,
             'logo' => $this->logo,
             'status' => $this->status,
+            'parent' => new Organization(OrganizationEntity::find($this->parent_id)),
+//            'children' => new Organization(OrganizationEntity::where('parent_id', $this->id)->first()),
             'createdAt' => $this->created_at,
             'updatedAt' => $this->updated_at,
         ];

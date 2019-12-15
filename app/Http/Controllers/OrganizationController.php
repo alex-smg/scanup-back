6f7f9788558f518bf150f528af56966d94ba44e6
j.bourel.ext_azu <calaruni@hotmail.com>
2019-12-15 17:11:01 +0100

    feat(organizations):Add store method for create organization

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Organization;
+use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\{Response, DB, Validator};
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use App\Http\Resources\Organization as OrganizationResource;
 
@@ -23,5 +24,21 @@ class OrganizationController extends Controller
     public function show(int $id): OrganizationResource
     {
         return new OrganizationResource(Organization::find($id));
+    }
+
+    public function store(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            'name' => 'required|string|min:1|max:255',
+//            'logo' => 'image'
+            'status' => ['required', Rule::in(['organization', 'company', 'brand'])]
+        ]);
+
+        if ($validation->fails())
+            return $validation->errors();
+
+        $organization = Organization::create($request->only(['name', 'description', 'logo', 'status', 'parent_id']));
+
+        return Response::json($organization, 201);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ $router->group(['prefix' => 'api'], function () use ($router) {
 
     $router->get('/organizations', 'OrganizationController@index');
     $router->get('/organizations/{id}', 'OrganizationController@show');
+    $router->post('/organizations', 'OrganizationController@store');
+    $router->patch('/organizations/{id}', 'OrganizationController@update');
+    $router->delete('/organizations/{id}', 'OrganizationController@delete');
 
 });
 

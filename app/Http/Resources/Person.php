60758517ee1828fabcd3798eccaefb36a6326f61
j.bourel.ext_azu <calaruni@hotmail.com>
2019-12-26 21:22:58 +0100

    feat(person): Add keys and values to person resource

--- app/Http/Resources/Person.php
+++ app/Http/Resources/Person.php
@@ -14,6 +14,11 @@ class Person extends Resource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'email' => $this->email,
+            'organization_id' => $this->organization,
+        ];
     }
 }

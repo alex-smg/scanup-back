c24bd5728a904df182b1670d460b66f881a95726
j.bourel.ext_azu <calaruni@hotmail.com>
2019-12-27 21:18:32 +0100

    feat(person): Featch all users

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Person as PersonResource;
+use App\Person;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class PersonController extends Controller
 {
-    //
+    /**
+     * @return AnonymousResourceCollection
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        return PersonResource::collection(Person::paginate(10));
+    }
 }

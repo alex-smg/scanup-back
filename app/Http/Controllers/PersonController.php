d952873ded5ce7b167bc585bf0655fc0c971bc29
j.bourel.ext_azu <calaruni@hotmail.com>
2019-12-29 19:24:06 +0100

    feat(person): Show one person by id

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Organization as OrganizationResource;
 use App\Http\Resources\Person as PersonResource;
+use App\Organization;
 use App\Person;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
@@ -15,5 +17,14 @@ class PersonController extends Controller
     public function index(): AnonymousResourceCollection
     {
         return PersonResource::collection(Person::paginate(10));
+    }
+
+    /**
+     * @param int $id
+     * @return PersonResource
+     */
+    public function show(int $id): PersonResource
+    {
+        return new PersonResource(Person::find($id));
     }
 }

19418b749a0e67ec0890372e22b40ba5e1026470
j.bourel.ext_azu <calaruni@hotmail.com>
2019-12-30 19:31:52 +0100

    feat(person): Add method for remove one person

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\Organization as OrganizationResource;
 use App\Http\Resources\Person as PersonResource;
-use App\Organization;
 use App\Person;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
 
 class PersonController extends Controller
 {
@@ -26,5 +28,16 @@ class PersonController extends Controller
     public function show(int $id): PersonResource
     {
         return new PersonResource(Person::find($id));
+    }
+
+    /**
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function destroy(int $id): JsonResponse
+    {
+        DB::table('persons')->where('id', '=', $id)->delete();
+
+        return Response::json([], 204);
     }
 }

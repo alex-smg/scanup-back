1d390f0fc5c1f8a8aa223b69523422f1235eeed7
j.bourel.ext_azu <calaruni@hotmail.com>
2020-01-02 21:12:43 +0100

    feat(person): Add new method for update a person

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\Organization as OrganizationResource;
 use App\Http\Resources\Person as PersonResource;
 use App\Person;
 use Illuminate\Http\JsonResponse;
@@ -41,7 +40,7 @@ class PersonController extends Controller
             'first_name' => 'required|string|min:1|max:255',
             'last_name' => 'required|image',
             'email' => 'integer',
-            'organization_id' => ['required']
+            'organization_id' => 'required'
         ]);
 
         if ($validation->fails())
@@ -50,6 +49,30 @@ class PersonController extends Controller
         $person = $request->only(['name', 'description', 'status', 'parent_id']);
 
         return Response::json($person, 201);
+    }
+
+    /**
+     * @param Request $request
+     * @param int $id
+     * @return JsonResponse|\Illuminate\Support\MessageBag
+     */
+    public function update(Request $request, int $id)
+    {
+        $validation = Validator::make($request->all(), [
+            'first_name' => 'required|string|min:1|max:255',
+            'last_name' => 'required|image',
+            'email' => 'integer',
+            'organization_id' => 'required'
+        ]);
+
+        if ($validation->fails())
+            return $validation->errors();
+
+        $dataToInsert = $request->only(['name', 'description', 'status', 'parent_id']);
+
+        Person::where('id', $id)->update($dataToInsert);
+
+        return Response::json(Person::where('id', $id)->get(), 200);
     }
 
     /**

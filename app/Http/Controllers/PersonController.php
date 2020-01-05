eff8884ea5f6c95ab0dde7b3f9b8bb38ce0d1815
j.bourel.ext_azu <calaruni@hotmail.com>
2020-01-05 18:47:33 +0100

    feat(person): Add the method for login an user

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\Person as PersonResource;
 use App\Person;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Resources\Person as PersonResource;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class PersonController extends Controller
 {
@@ -63,7 +64,7 @@ class PersonController extends Controller
         $validation = Validator::make($request->all(), [
             'first_name' => 'required|string|min:1|max:255',
             'last_name' => 'required|image',
-            'email' => 'integer',
+            'email' => 'required|email|max:255',
             'organization_id' => 'required'
         ]);
 
@@ -87,4 +88,40 @@ class PersonController extends Controller
 
         return Response::json([], 204);
     }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Auth\Authenticatable|JsonResponse|\Illuminate\Support\MessageBag|null
+     */
+    public function login(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            'email' => 'required|string|email|min:2|max:255',
+            'password' => 'required|string|min:6|max:255'
+        ]);
+
+        if ($validation->fails())
+            return $validation->errors();
+
+        if ($this->guard()->attempt(['email' => $request->input('email'), 'password' => $request->input('password')])) {
+            $user = $this->guard()->user();
+            $username = $user->email;
+            $password = $request->input('password');
+            $basicAuth = base64_encode("{$username}:{$password}");
+            $user->token = $basicAuth;
+
+            return $user;
+        }
+
+        return Response::json(['error' => 'Unauthenticated user'], 401);
+    }
+
+    /**
+     * @return Auth
+     */
+    private function guard(): Auth
+    {
+        return Auth::guard('api');
+    }
+
 }

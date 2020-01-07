9a2f014fca771ac9a2af2d7d5355692f269bc091
j.bourel.ext_azu <calaruni@hotmail.com>
2020-01-07 22:24:46 +0100

    feat(laravel-permission): Install and add the configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -81,6 +81,15 @@ $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
+$app->routeMiddleware([
+    'auth'       => App\Http\Middleware\Authenticate::class,
+    'permission' => Spatie\Permission\Middlewares\PermissionMiddleware::class,
+    'role'       => Spatie\Permission\Middlewares\RoleMiddleware::class,
+]);
+$app->configure('permission');
+$app->alias('cache', \Illuminate\Cache\CacheManager::class);  // if you don't have this already
+$app->register(Spatie\Permission\PermissionServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes

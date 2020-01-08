e8bb20b0d90315459e9787b8164d10c7138a1951
j.bourel.ext_azu <calaruni@hotmail.com>
2020-01-08 20:00:34 +0100

    feat(person): Edit seed for add a super-admin role to each user

--- app/Person.php
+++ app/Person.php
@@ -3,9 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Contracts\Auth\Access\Authorizable as Authenticatable;
 
-class Person extends Model
+
+class Person extends Model implements Authenticatable
 {
+    use Authorizable;
+    use HasRoles;
+
     protected $table = 'persons';
     protected $fillable = ['first_name', 'last_name', 'email', 'password', 'organization_id'];
 
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\{Organization, Person, Question, Response, Survey};
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,14 +14,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        Role::create(['name' => 'super-admin']);
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'moderator']);
+        Role::create(['name' => 'viewer']);
+
         factory(Organization::class, 22)->create()->each(function($organization, $key) {
             if ($key % 2 === 0 || $key % 3 === 0 ) {
                 $organization->parent()->associate(factory(Organization::class)->create());
             }
         });
-        factory(Person::class, 10)->create();
+        factory(Person::class, 10)->create()->each(function($person) {
+            $person->assignRole('super-admin');
+        });
         factory(Question::class, 20)->create();
         factory(Response::class, 50)->create();
         factory(Survey::class, 10)->create();
+
     }
 }

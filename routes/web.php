c4625f5e68d88ada8675b2c6c4fb22d61db5be1e
j.bourel.ext_azu <calaruni@hotmail.com>
2020-01-10 00:03:16 +0100

    feat(routes): Add middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -26,21 +26,23 @@ $router->group(['prefix' => 'api'], function() use($router) {
 
     $router->post('/login', 'PersonController@login');
 
-    $router->group(['middleware' => ['jwt.auth', 'role:super-admin|admin']], function() use ($router) {
+    $router->group(['middleware' => ['jwt.auth']], function() use ($router) {
+        $router->get('/persons', 'PersonController@index');
+        $router->get('/persons/{id}', 'PersonController@show');
+
+        $router->get('/organizations/{id}', 'OrganizationController@show');
         $router->get('/organizations', 'OrganizationController@index');
     });
 
-    $router->group(['middleware' => 'jwt.auth'], function() use ($router) {
-        $router->get('/organizations/{id}', 'OrganizationController@show');
-        $router->post('/organizations', 'OrganizationController@store');
-        $router->put('/organizations/{id}', 'OrganizationController@update');
-        $router->delete('/organizations/{id}', 'OrganizationController@destroy');
 
-        $router->get('/persons', 'PersonController@index');
-        $router->get('/persons/{id}', 'PersonController@show');
+    $router->group(['middleware' => ['jwt.auth', 'role:admin']], function() use ($router) {
         $router->post('/persons', 'PersonController@store');
         $router->put('/persons/{id}', 'PersonController@update');
         $router->delete('/persons/{id}', 'PersonController@destroy');
+
+        $router->post('/organizations', 'OrganizationController@store');
+        $router->put('/organizations/{id}', 'OrganizationController@update');
+        $router->delete('/organizations/{id}', 'OrganizationController@destroy');
     });
 });
 

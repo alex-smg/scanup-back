e31dd3860f81ae8e0e3a4b0f33cca9dec0f4eec3
j.bourel.ext_azu <calaruni@hotmail.com>
2020-02-21 16:58:06 +0100

    feat(question): Add image field in the table question

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -8,6 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Question::class, function (Faker $faker) {
     return [
         'title' => $faker->text($maxNbChars = 250),
+        'image' => $faker->imageUrl($width = 640, $height = 480),
         'multi_choice' => $faker->boolean($chanceOfGettingTrue = 50),
         'survey_id' => factory('App\Survey')->create()->id
     ];

93c71d59bb6624b4a459b60bb9b7dd5eea3d6245
j.bourel.ext_azu <calaruni@hotmail.com>
2020-02-22 17:23:40 +0100

    refactor(route): Move the route for create person in the middleware that check if the customer has a role admin

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ $router->get('/', function () use ($router) {
 
 $router->group(['prefix' => 'api'], function() use($router) {
     $router->post('/login', 'PersonController@login');
-    $router->post('/persons', 'PersonController@store');
 
     $router->group(['middleware' => ['jwt.auth']], function() use ($router) {
         $router->get('/persons', 'PersonController@index');
@@ -38,6 +37,7 @@ $router->group(['prefix' => 'api'], function() use($router) {
 
 
     $router->group(['middleware' => ['jwt.auth', 'role:admin']], function() use ($router) {
+        $router->post('/persons', 'PersonController@store');
         $router->put('/persons/{id}', 'PersonController@update');
         $router->delete('/persons/{id}', 'PersonController@destroy');
 

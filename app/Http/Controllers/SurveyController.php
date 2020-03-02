53e341e6985099be846fb2f3a9a4e6207d0117ec
Bourel <jrmbourel@gmail.com>
2020-03-02 23:50:52 +0100

    refactor(survey): Return 6 results maximum by search

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -49,7 +49,7 @@ class SurveyController extends Controller
      */
     public function search(string $value)
     {
-        return Survey::where('title', 'like', '%'.$value.'%')->get();
+        return Survey::where('title', 'like', '%'.$value.'%')->paginate(6);
     }
 
     /**
